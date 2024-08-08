cc75e8f91a7a7f49ff1501ceb216bf4a5b946397
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2024-08-08 15:11:18 +0600

    errors

---
+++ error.js
@@ -0,0 +1,48 @@
+// Syntax Error
+
+console.log( 10;
+
+Let x = 10;
+
+cost x = 10;
+
+const arr = [ 1, 2, 3; 4, 5];
+
+function sum(a, b {
+    return a + b;
+}
+
+
+//Type Error
+
+let name;
+console.log(name.length);
+
+
+let arr = [ 1, 2, 3, 4, 5 ];
+arr();
+
+
+//Reference Error
+
+function sum(a, b) {
+    const result = a + b;
+    return result;
+}
+
+console.log( result );
+
+
+if(true) {
+    console.log( name );
+    let name = 'John';
+}
+
+
+
+const cars = {
+    brand: 'Ford'
+
+}
+
+cars.map( car => console.log(car));
